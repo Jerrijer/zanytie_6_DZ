f9f028dd0a39300f9f84a31ef16597161636690a
Evgeniy Abirkin <infoshoco@yandex.ru>
2023-04-29 10:05:19 +0300

    ДЗ 6

--- testHomework6.py
+++ testHomework6.py
@@ -3,72 +3,43 @@ import inspect
 from datetime import time
 
 
-
-
-
-
-
 def test_dark_theme_by_time():
 
     """
-
     Протестируйте правильность переключения темной темы на сайте в зависимости от времени
-
     """
 
     current_time = time(hour=23)
-
     if current_time.hour < 7 or current_time.hour > 22:
-
         is_dark_theme = True
     assert is_dark_theme is True
-
 
 
 
 def test_dark_theme_by_time_and_user_choice():
 
     """
-
     Протестируйте правильность переключения темной темы на сайте
-
     в зависимости от времени и выбора пользователя
-
     dark_theme_enabled_by_user = True - Темная тема включена
-
     dark_theme_enabled_by_user = False - Темная тема выключена
-
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
-
     """
 
     current_time = time(hour=16)
-
     dark_theme_enabled_by_user = True
 
 
-
-
     if current_time.hour < 7 or current_time.hour > 22:
-
         if dark_theme_enabled_by_user:
-
             is_dark_theme = True
-
         else:
-
             is_dark_theme = False
-
     elif dark_theme_enabled_by_user:
-
         is_dark_theme = True
-
     else:
-
         is_dark_theme = False
-
     assert is_dark_theme is True
-
 
 
 
@@ -88,17 +59,13 @@ def test_find_suitable_user():
 
 
     suitable_users = [k for k in users for r, t in k.items() if t == 'Olga'][0]
-
     assert suitable_users == {"name": "Olga", "age": 45}
-
-
     suitable_users = []
 
     for k in users:
         for r, t in k.items():
             if type(t) == int and t < 20:
                 suitable_users.append(k)
-
 
     assert suitable_users == [
         {"name": "Stanislav", "age": 15},
@@ -121,11 +88,9 @@ def test_find_suitable_user():
 
 
 def test_readable_function():
-
     open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
-
 
     print(inspect.getfullargspec(find_registration_button_on_login_page)[0])
 
@@ -135,18 +100,15 @@ def print_a_name(input1, input2):
     return f'{input1} [{input2}]'
 
 
-
 def open_browser(browser_name):
     actual_result = print_a_name(open_browser.__name__, *inspect.getfullargspec(open_browser)[0])
     assert actual_result == "Open Browser [Chrome]"
-
 
 
 def go_to_companyname_homepage(page_url):
     actual_result = print_a_name(go_to_companyname_homepage.__name__,
     *inspect.getfullargspec(go_to_companyname_homepage)[0])
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
-
 
 
 def find_registration_button_on_login_page(page_url, button_text):
